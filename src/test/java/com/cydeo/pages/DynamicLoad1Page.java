c84f2057cb2974f0909754d35a29e3f6fc03f2b1
gurhank <gurhankocyigit@gmail.com>
2022-03-11 15:43:13 -0600

    day12 WebDriveWaitPractices.java

--- src/test/java/com/cydeo/pages/DynamicLoad1Page.java
+++ src/test/java/com/cydeo/pages/DynamicLoad1Page.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DynamicLoad1Page {
@@ -8,5 +10,14 @@ public class DynamicLoad1Page {
     public DynamicLoad1Page(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+
+    @FindBy(xpath = "//button[.='Start']")
+    public WebElement startButton;
+
+    @FindBy(css = "div#loading")
+    public WebElement loadingBar;
+
+
 
 }

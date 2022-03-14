c5738a618fa7306c30d759ff0f0f2a31c731e401
gurhank <gurhankocyigit@gmail.com>
2022-03-14 13:29:04 -0500

    day13 ExplicitWaitPractices.java

--- src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
+++ src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
@@ -55,8 +55,22 @@ public class ExplicitWaitPractices {
     public void enable_button_test(){
         //3- Click to “Enable” button
 
+        //System.out.println("dynamicControlsPage.inputBox.isEnabled() = "
+        //        + dynamicControlsPage.inputBox.isEnabled());
+
+        dynamicControlsPage.enableButton.click();
+
+        System.out.println("dynamicControlsPage.inputBox.isEnabled() = "
+                + dynamicControlsPage.inputBox.isEnabled());
+
 
         //4- Wait until “loading bar disappears”
+        //Calling our ExplicitWait utility method to wait loadingBar to disappear
+        BrowserUtils.waitForInvisibilityOf(dynamicControlsPage.loadingBar);
+
+        System.out.println("dynamicControlsPage.inputBox.isEnabled() = "
+                + dynamicControlsPage.inputBox.isEnabled());
+
         //5- Verify:
         //a. Input box is enabled.
         //b. “It’s enabled!” message is displayed.
